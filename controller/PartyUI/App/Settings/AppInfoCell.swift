95fabfa37997fc1746885d91d96a652803e3370c
Bot man <clogan9019@gmail.com>
2026-05-22 13:58:15 -0400

    fix: remove glassEffect (unavailable in Xcode 16.2 SDK) from AppInfoCell

--- controller/PartyUI/App/Settings/AppInfoCell.swift
+++ controller/PartyUI/App/Settings/AppInfoCell.swift
@@ -2,14 +2,12 @@
 //  AppInfoCell.swift
 //  PartyUI
 //
-//  Created by lunginspector on 3/3/26.
-//
 
 import SwiftUI
 
 public struct AppInfoCell: View {
     public init() {}
-    
+
     public var body: some View {
         HStack(spacing: 14) {
             AppIcon()
@@ -23,34 +21,23 @@ public struct AppInfoCell: View {
     }
 }
 
-// icon for AppInfoCell
 public struct AppIcon: View {
     var image: Image
-    
+
     init(image: Image = Image(uiImage: AppInfo.appIcon ?? UIImage())) {
         self.image = image
     }
-    
+
     public var body: some View {
-        if #available(iOS 19.0, *) {
-            image
-                .resizable()
-                .scaledToFit()
-                .frame(width: 64, height: 64)
-                .background(Color(.systemGray6))
-                .clipShape(.rect(cornerRadius: 18))
-                .glassEffect(.regular, in: .rect(cornerRadius: 18))
-        } else {
-            image
-                .resizable()
-                .scaledToFit()
-                .frame(width: 64, height: 64)
-                .background(Color(.systemGray6))
-                .clipShape(.rect(cornerRadius: 14))
-                .overlay {
-                    RoundedRectangle(cornerRadius: 14)
-                        .stroke(Color.primary.opacity(0.2), lineWidth: 2)
-                }
-        }
+        image
+            .resizable()
+            .scaledToFit()
+            .frame(width: 64, height: 64)
+            .background(Color(.systemGray6))
+            .clipShape(.rect(cornerRadius: 14))
+            .overlay {
+                RoundedRectangle(cornerRadius: 14)
+                    .stroke(Color.primary.opacity(0.2), lineWidth: 2)
+            }
     }
 }
