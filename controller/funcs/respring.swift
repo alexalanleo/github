e2ec1539bdadf822ce61b764c9e5b7e0e247f3d9
Bot man <clogan9019@gmail.com>
2026-05-22 13:58:38 -0400

    fix: add respring() function (was only respringview struct)

--- controller/funcs/respring.swift
+++ controller/funcs/respring.swift
@@ -2,13 +2,6 @@
 //  respring.swift
 //  controller
 //
-//  Created by ruter on 07.05.26.
-//  skidded from jailbreak.party
-//
-
-// Respring.swift: easy respring on all iOS versions (probably).
-// Web approach developed by @neonmodder123, implemented in Swift here by @skadz108.
-// 4/9/26, https://jailbreak.party
 
 import Foundation
 import SwiftUI
@@ -19,7 +12,6 @@ let respringdoc = """
 <!DOCTYPE html>
 <html>
     <body>
-        <!--  big credit to @neonmodder123  -->
         <iframe id="frame" srcdoc="" sandbox="allow-forms allow-modals allow-orientation-lock allow-pointer-lock allow-popups allow-presentation allow-scripts"></iframe>
         <script>
             const frame = document.getElementById('frame');
@@ -30,13 +22,11 @@ let respringdoc = """
                         const container = document.createElement('div');
                         container.style.cssText = 'perspective: 1px; perspective-origin: 9999999% 9999999%;';
                         document.body.appendChild(container);
-    
                         for (let i = 0; i < 500; i++) {
                             let d = document.createElement('div');
                             d.style.cssText = 'position: absolute; width: 100vw; height: 100vh; backdrop-filter: blur(100px); -webkit-backdrop-filter: blur(100px); transform: translate3d(100000px, 100000px, ' + i + 'px) rotateY(90deg);';
                             container.appendChild(d);
                         }
-    
                         setInterval(() => {
                             navigator.share({ title: 'R', text: 'R'.repeat(100000) }).catch(() => {});
                             let x = new Uint8Array(1024 * 1024 * 10);
@@ -46,20 +36,29 @@ let respringdoc = """
                 </body>
                 </html>
             `;
-    
             frame.srcdoc = script;
         </script>
     </body>
 </html>
 """
 
+func respring() {
+    DispatchQueue.main.async {
+        guard let window = UIApplication.shared.connectedScenes
+            .compactMap({ $0 as? UIWindowScene })
+            .flatMap({ $0.windows })
+            .first(where: { $0.isKeyWindow }) else { return }
+        let wv = WKWebView(frame: window.bounds)
+        wv.loadHTMLString(respringdoc, baseURL: nil)
+        window.addSubview(wv)
+    }
+}
+
 struct respringview: UIViewRepresentable {
     func makeUIView(context: Context) -> WKWebView {
         let ww = WKWebView()
-        WKWebpagePreferences().allowsContentJavaScript = true
         return ww
     }
-
     func updateUIView(_ ww: WKWebView, context: Context) {
         ww.loadHTMLString(respringdoc, baseURL: nil)
     }
