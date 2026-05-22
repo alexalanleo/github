036a4316ade4992e343ae6e3894fe3d06e936b1c
Bot man <clogan9019@gmail.com>
2026-05-22 13:58:25 -0400

    fix: remove glassEffect from LinkCreditCell

--- controller/PartyUI/App/Settings/LinkCreditCell.swift
+++ controller/PartyUI/App/Settings/LinkCreditCell.swift
@@ -2,8 +2,6 @@
 //  LinkCreditCell.swift
 //  PartyUI
 //
-//  Created by lunginspector on 3/3/26.
-//
 
 import SwiftUI
 
@@ -13,14 +11,14 @@ public struct LinkCreditCell<Icon: View>: View {
     var description: String
     var url: String
     @Environment(\.openURL) var openURL
-    
+
     public init(name: String, description: String, url: String = "", @ViewBuilder image: () -> Icon) {
         self.image = image()
         self.name = name
         self.description = description
         self.url = url
     }
-    
+
     public var body: some View {
         Button(action: {
             if !url.isEmpty { openURL(URL(string: url)!) }
@@ -48,44 +46,28 @@ public struct LinkCreditCell<Icon: View>: View {
     }
 }
 
-// icon for credits cell
 public struct LinkCreditIcon: View {
     var url: String
-    
+
     init(url: String) {
         self.url = url
     }
-    
+
     public var body: some View {
-        if #available(iOS 19.0, *) {
-            AsyncImage(url: URL(string: url)) { image in
-                image
-                    .resizable()
-                    .scaledToFill()
-                    .frame(width: 40, height: 40)
-                    .background(Color(.systemGray6))
-                    .clipShape(Circle())
-                    .glassEffect(.regular, in: Circle())
-            } placeholder: {
-                ProgressView()
-                    .frame(width: 40, height: 40)
-            }
-        } else {
-            AsyncImage(url: URL(string: url)) { image in
-                image
-                    .resizable()
-                    .scaledToFill()
-                    .frame(width: 40, height: 40)
-                    .background(Color(.systemGray6))
-                    .clipShape(Circle())
-                    .overlay {
-                        Circle()
-                            .stroke(Color.primary.opacity(0.2), lineWidth: 1)
-                    }
-            } placeholder: {
-                ProgressView()
-                    .frame(width: 40, height: 40)
-            }
+        AsyncImage(url: URL(string: url)) { image in
+            image
+                .resizable()
+                .scaledToFill()
+                .frame(width: 40, height: 40)
+                .background(Color(.systemGray6))
+                .clipShape(Circle())
+                .overlay {
+                    Circle()
+                        .stroke(Color.primary.opacity(0.2), lineWidth: 1)
+                }
+        } placeholder: {
+            ProgressView()
+                .frame(width: 40, height: 40)
         }
     }
 }
