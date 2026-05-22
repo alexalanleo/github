22b14e231267abe3b1a51153cc5f971c56093a73
Bot man <clogan9019@gmail.com>
2026-05-22 14:28:30 -0400

    feat: install CrashProtection at app startup

--- controller/controller.swift
+++ controller/controller.swift
@@ -1,50 +1,52 @@
 //
-  //  controller.swift
-  //  controller
-  //
+//  controller.swift
+//  controller
+//
 
-  import SwiftUI
-  import UniformTypeIdentifiers
+import SwiftUI
+import UniformTypeIdentifiers
 
-  enum taboptions {
-      case home, installer, root, logs
-  }
+enum taboptions {
+    case home, installer, root, logs
+}
 
-  let g_isunsupported: Bool = isunsupported()
-  var g_debugbuild: Bool = false
+let g_isunsupported: Bool = isunsupported()
+var g_debugbuild: Bool = false
 
-  @main
-  struct controller: App {
-      @StateObject private var mgr = controllermgr.shared
-      @Environment(\.scenePhase) var scenephase
-      @AppStorage("keepAlive") private var keepalive: Bool = false
+@main
+struct controller: App {
+    @StateObject private var mgr = controllermgr.shared
+    @Environment(\.scenePhase) var scenephase
+    @AppStorage("keepAlive") private var keepalive: Bool = false
 
-      init() {
-          #if DEBUG
-          g_debugbuild = true
-          #endif
+    init() {
+        CrashProtection.install()
 
-          // fix file picker
-          let fixMethod = class_getInstanceMethod(UIDocumentPickerViewController.self, #selector(UIDocumentPickerViewController.fix_init(forOpeningContentTypes:asCopy:)))!
-          let origMethod = class_getInstanceMethod(UIDocumentPickerViewController.self, #selector(UIDocumentPickerViewController.init(forOpeningContentTypes:asCopy:)))!
-          method_exchangeImplementations(origMethod, fixMethod)
+        #if DEBUG
+        g_debugbuild = true
+        #endif
 
-          if keepalive { toggleka() }
-          globallogger.capture()
-      }
+        // fix file picker
+        let fixMethod = class_getInstanceMethod(UIDocumentPickerViewController.self, #selector(UIDocumentPickerViewController.fix_init(forOpeningContentTypes:asCopy:)))!
+        let origMethod = class_getInstanceMethod(UIDocumentPickerViewController.self, #selector(UIDocumentPickerViewController.init(forOpeningContentTypes:asCopy:)))!
+        method_exchangeImplementations(origMethod, fixMethod)
 
-      var body: some Scene {
-          WindowGroup {
-              ContentView()
-                  .environmentObject(mgr)
-                  .preferredColorScheme(.dark)
-          }
-      }
-  }
+        if keepalive { toggleka() }
+        globallogger.capture()
+    }
 
-  extension UIDocumentPickerViewController {
-      @objc func fix_init(forOpeningContentTypes contentTypes: [UTType], asCopy: Bool) -> UIDocumentPickerViewController {
-          return self.fix_init(forOpeningContentTypes: contentTypes, asCopy: true)
-      }
-  }
-  
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environmentObject(mgr)
+                .preferredColorScheme(.dark)
+        }
+    }
+}
+
+extension UIDocumentPickerViewController {
+    @objc func fix_init(forOpeningContentTypes contentTypes: [UTType], asCopy: Bool) -> UIDocumentPickerViewController {
+        return self.fix_init(forOpeningContentTypes: contentTypes, asCopy: true)
+    }
+}
+
