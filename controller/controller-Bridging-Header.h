da6311e28bf52c9d6804ef8cf51370974ce11648
Bot man <clogan9019@gmail.com>
2026-05-22 20:38:19 -0400

    anti-crash: import ExploitGuard.h in bridging header

--- controller/controller-Bridging-Header.h
+++ controller/controller-Bridging-Header.h
@@ -1,45 +1,45 @@
 //
-  //  controller-Bridging-Header.h
-  //  controller
-  //
+//  controller-Bridging-Header.h
+//  controller
+//
 
-  @import UIKit;
-  #import <Foundation/Foundation.h>
+@import UIKit;
+#import <Foundation/Foundation.h>
 
-  #import "darksword.h"
-  #import "offsets.h"
-  #import "utils.h"
-  #import "vnode.h"
-  #import "apfs.h"
-  #import "vfs.h"
-  #import "sbx.h"
-  #import "rc.h"
-  #import "RemoteCall.h"
+#import "darksword.h"
+#import "offsets.h"
+#import "utils.h"
+#import "vnode.h"
+#import "apfs.h"
+#import "vfs.h"
+#import "sbx.h"
+#import "rc.h"
+#import "RemoteCall.h"
+#import "ExploitGuard.h"
 
-  // Root management
-  int grant_root_to_pid(pid_t pid);
-  int revoke_root_from_pid(pid_t pid);
+// Root management
+int grant_root_to_pid(pid_t pid);
+int revoke_root_from_pid(pid_t pid);
 
-  // IPA installer
-  int install_app_bundle(const char *appBundlePath);
-  int uninstall_app(const char *bundleID);
+// IPA installer
+int install_app_bundle(const char *appBundlePath);
+int uninstall_app(const char *bundleID);
 
-  // VFS
-  int vfs_init(void);
+// VFS
+int vfs_init(void);
 
-  long findcachedataoff(const char *mgkey);
-  void LaraClearIconCache(void);
+long findcachedataoff(const char *mgkey);
+void LaraClearIconCache(void);
 
-  @interface UIDevice(Private)
-  + (BOOL)_hasHomeButton;
-  @end
+@interface UIDevice(Private)
++ (BOOL)_hasHomeButton;
+@end
 
-  NS_ASSUME_NONNULL_BEGIN
+NS_ASSUME_NONNULL_BEGIN
 
-  @interface VarCleanBridge : NSObject
-  + (NSDictionary *)loadRulesNamed:(NSString *)resourceName inBundle:(NSBundle *)bundle error:(NSError * _Nullable * _Nullable)error;
-  + (BOOL)probePathExists:(NSString *)path isDirectory:(BOOL *)isDirectory isSymlink:(BOOL *)isSymlink;
-  @end
+@interface VarCleanBridge : NSObject
++ (NSDictionary *)loadRulesNamed:(NSString *)resourceName inBundle:(NSBundle *)bundle error:(NSError * _Nullable * _Nullable)error;
++ (BOOL)probePathExists:(NSString *)path isDirectory:(BOOL *)isDirectory isSymlink:(BOOL *)isSymlink;
+@end
 
-  NS_ASSUME_NONNULL_END
-  
+NS_ASSUME_NONNULL_END
