e9697e250b29a750a1b56e4ac8d973b114ac1cc9
Bot man <clogan9019@gmail.com>
2026-05-27 08:34:14 -0400

    Fix IPA installer: root grant, permissions, fallback registration

--- controller/classes/IPAInstaller.m
+++ controller/classes/IPAInstaller.m
@@ -1,76 +1,135 @@
 //
-  //  IPAInstaller.m
-  //  controller
-  //
-  //  Installs app bundles using VFS access provided by DarkSword.
-  //
+//  IPAInstaller.m
+//  controller
+//
+//  Installs app bundles using VFS access provided by DarkSword.
+//
 
-  #import <Foundation/Foundation.h>
-  #include "vfs.h"
-  #include "darksword.h"
-  #import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
+#include "vfs.h"
+#include "darksword.h"
+#include "root.h"
+#import <UIKit/UIKit.h>
+#import <objc/message.h>
 
-  // Install an extracted .app bundle to /var/containers/Bundle/Application/
-  int install_app_bundle(const char *appBundlePath) {
-      if (!ds_is_ready()) return -1;
+// Recursively set permissions on the installed .app bundle
+static void fix_permissions_recursive(NSString *path) {
+    NSFileManager *fm = [NSFileManager defaultManager];
+    NSDictionary *dirAttrs  = @{NSFilePosixPermissions: @(0755)};
+    NSDictionary *fileAttrs = @{NSFilePosixPermissions: @(0644)};
+    [fm setAttributes:dirAttrs ofItemAtPath:path error:nil];
+    NSDirectoryEnumerator *en = [fm enumeratorAtPath:path];
+    NSString *item;
+    while ((item = [en nextObject])) {
+        NSString *full = [path stringByAppendingPathComponent:item];
+        BOOL isDir = NO;
+        [fm fileExistsAtPath:full isDirectory:&isDir];
+        [fm setAttributes:(isDir ? dirAttrs : fileAttrs) ofItemAtPath:full error:nil];
+    }
+}
 
-      NSString *srcPath = [NSString stringWithUTF8String:appBundlePath];
-      NSString *appsDir = @"/var/containers/Bundle/Application";
-      NSString *destUUID = [[NSUUID UUID] UUIDString];
-      NSString *destDir = [appsDir stringByAppendingPathComponent:destUUID];
-      NSString *destApp = [destDir stringByAppendingPathComponent:[srcPath lastPathComponent]];
+// Install an extracted .app bundle to /var/containers/Bundle/Application/
+int install_app_bundle(const char *appBundlePath) {
+    if (!ds_is_ready()) return -1;
 
-      NSFileManager *fm = [NSFileManager defaultManager];
-      NSError *err = nil;
+    // Need root to write into /var/containers/Bundle/Application/
+    int rootErr = grant_root_to_pid(getpid());
+    if (rootErr != 0) {
+        NSLog(@"[controller] grant_root_to_pid failed: %d", rootErr);
+        return -10;
+    }
 
-      // Create container dir
-      if (![fm createDirectoryAtPath:destDir withIntermediateDirectories:YES attributes:nil error:&err]) {
-          NSLog(@"[controller] Failed to create app dir: %@", err);
-          return -2;
-      }
+    NSString *srcPath = [NSString stringWithUTF8String:appBundlePath];
+    NSString *appsDir = @"/var/containers/Bundle/Application";
+    NSString *destUUID = [[NSUUID UUID] UUIDString];
+    NSString *destDir  = [appsDir stringByAppendingPathComponent:destUUID];
+    NSString *destApp  = [destDir stringByAppendingPathComponent:[srcPath lastPathComponent]];
 
-      // Copy app bundle
-      if (![fm copyItemAtPath:srcPath toPath:destApp error:&err]) {
-          NSLog(@"[controller] Failed to copy app bundle: %@", err);
-          return -3;
-      }
+    NSFileManager *fm = [NSFileManager defaultManager];
+    NSError *err = nil;
 
-      // Fix permissions
-      NSDictionary *attrs = @{NSFilePosixPermissions: @(0755)};
-      [fm setAttributes:attrs ofItemAtPath:destApp error:nil];
+    // Create container directory
+    if (![fm createDirectoryAtPath:destDir withIntermediateDirectories:YES attributes:nil error:&err]) {
+        NSLog(@"[controller] Failed to create app dir: %@", err);
+        revoke_root_from_pid(getpid());
+        return -2;
+    }
 
-      // Register in LSApplicationWorkspace (private API via RemoteCall or direct call)
-      Class workspace = NSClassFromString(@"LSApplicationWorkspace");
-      id ws = [workspace performSelector:@selector(defaultWorkspace)];
-      BOOL result = [[ws performSelector:@selector(registerApplicationDictionary:)
-                              withObject:@{@"Path": destApp, @"ApplicationType": @"User"}] boolValue];
+    // Copy the .app bundle
+    if (![fm copyItemAtPath:srcPath toPath:destApp error:&err]) {
+        NSLog(@"[controller] Failed to copy app bundle: %@", err);
+        revoke_root_from_pid(getpid());
+        return -3;
+    }
 
-      // Track installed app
-      NSString *infoPlistPath = [destApp stringByAppendingPathComponent:@"Info.plist"];
-      NSDictionary *info = [NSDictionary dictionaryWithContentsOfFile:infoPlistPath];
-      NSString *bundleID = info[@"CFBundleIdentifier"] ?: @"unknown";
-      NSMutableArray *installed = [[NSUserDefaults.standardUserDefaults
-          stringArrayForKey:@"ctrl_installed_apps"] mutableCopy] ?: [NSMutableArray new];
-      if (![installed containsObject:bundleID]) [installed addObject:bundleID];
-      [NSUserDefaults.standardUserDefaults setObject:installed forKey:@"ctrl_installed_apps"];
+    // Fix permissions so SpringBoard can read it
+    fix_permissions_recursive(destApp);
 
-      NSLog(@"[controller] Installed %@ -> %@", bundleID, destApp);
-      return result ? 0 : -4;
-  }
+    // Read Info.plist for registration metadata
+    NSString *infoPlistPath = [destApp stringByAppendingPathComponent:@"Info.plist"];
+    NSDictionary *info      = [NSDictionary dictionaryWithContentsOfFile:infoPlistPath];
+    NSString *bundleID      = info[@"CFBundleIdentifier"]      ?: @"unknown";
+    NSString *bundleName    = info[@"CFBundleDisplayName"]
+                           ?: info[@"CFBundleName"]
+                           ?: bundleID;
+    NSString *version       = info[@"CFBundleShortVersionString"] ?: @"1.0";
 
-  int uninstall_app(const char *bundleID) {
-      NSString *bid = [NSString stringWithUTF8String:bundleID];
-      Class workspace = NSClassFromString(@"LSApplicationWorkspace");
-      id ws = [workspace performSelector:@selector(defaultWorkspace)];
-      BOOL result = [[ws performSelector:@selector(uninstallApplication:withOptions:)
-                              withObject:bid withObject:nil] boolValue];
+    // Revoke root before talking to SpringBoard services
+    revoke_root_from_pid(getpid());
 
-      if (result) {
-          NSMutableArray *installed = [[NSUserDefaults.standardUserDefaults
-              stringArrayForKey:@"ctrl_installed_apps"] mutableCopy] ?: [NSMutableArray new];
-          [installed removeObject:bid];
-          [NSUserDefaults.standardUserDefaults setObject:installed forKey:@"ctrl_installed_apps"];
-      }
-      return result ? 0 : -1;
-  }
-  
+    // Register with LSApplicationWorkspace
+    Class workspace = NSClassFromString(@"LSApplicationWorkspace");
+    id ws = [workspace performSelector:@selector(defaultWorkspace)];
+
+    NSDictionary *appDict = @{
+        @"Path":                destApp,
+        @"ApplicationType":     @"User",
+        @"CFBundleIdentifier":  bundleID,
+        @"CFBundleDisplayName": bundleName,
+        @"CFBundleVersion":     version,
+        @"IsDeletable":         @YES,
+        @"IsUpgradeable":       @YES,
+    };
+
+    BOOL registered = NO;
+    SEL regSel = NSSelectorFromString(@"registerApplicationDictionary:");
+    if ([ws respondsToSelector:regSel]) {
+        registered = ((BOOL (*)(id, SEL, id))objc_msgSend)(ws, regSel, appDict);
+        NSLog(@"[controller] registerApplicationDictionary: %@", registered ? @"YES" : @"NO");
+    }
+
+    if (!registered) {
+        // Fallback: force a full app database rebuild so SpringBoard picks it up
+        SEL rebuildSel = NSSelectorFromString(@"_LSPrivateRebuildApplicationDatabasesForSystemApps:registeringPlugins:");
+        if ([ws respondsToSelector:rebuildSel]) {
+            ((void (*)(id, SEL, BOOL, BOOL))objc_msgSend)(ws, rebuildSel, YES, YES);
+            registered = YES;
+            NSLog(@"[controller] triggered _LSPrivateRebuildApplicationDatabasesForSystemApps");
+        }
+    }
+
+    // Track in UserDefaults so our UI can list it
+    NSMutableArray *installed = [[NSUserDefaults.standardUserDefaults
+        stringArrayForKey:@"ctrl_installed_apps"] mutableCopy] ?: [NSMutableArray new];
+    if (![installed containsObject:bundleID]) [installed addObject:bundleID];
+    [NSUserDefaults.standardUserDefaults setObject:installed forKey:@"ctrl_installed_apps"];
+
+    NSLog(@"[controller] Installed %@ (%@) -> %@", bundleName, bundleID, destApp);
+    return registered ? 0 : -4;
+}
+
+int uninstall_app(const char *bundleID) {
+    NSString *bid = [NSString stringWithUTF8String:bundleID];
+    Class workspace = NSClassFromString(@"LSApplicationWorkspace");
+    id ws = [workspace performSelector:@selector(defaultWorkspace)];
+    BOOL result = [[ws performSelector:@selector(uninstallApplication:withOptions:)
+                            withObject:bid withObject:nil] boolValue];
+
+    if (result) {
+        NSMutableArray *installed = [[NSUserDefaults.standardUserDefaults
+            stringArrayForKey:@"ctrl_installed_apps"] mutableCopy] ?: [NSMutableArray new];
+        [installed removeObject:bid];
+        [NSUserDefaults.standardUserDefaults setObject:installed forKey:@"ctrl_installed_apps"];
+    }
+    return result ? 0 : -1;
+}
